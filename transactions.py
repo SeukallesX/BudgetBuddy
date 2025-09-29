ccddcccf9cf207b5f4009aa3cf0e090ba759b4f8
Mariel Bravo <143050230+SeukallesX@users.noreply.github.com>
2025-09-29 10:24:54 -0700

    Delete transactions.py

--- transactions.py
+++
@@ -1,9 +0,0 @@
-# transactions.py
-
-class Transaction:
-    def __init__(self, date, t_type, category, amount, balance):
-        self.date = date
-        self.t_type = t_type      # "Income" or "Expense"
-        self.category = category  # Food, Rent, Shopping, Other
-        self.amount = amount
-        self.balance = balance
